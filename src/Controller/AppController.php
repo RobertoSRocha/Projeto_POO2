f828f2d0ec68ebb011b9c1a4abec012bcb6b5433
roberto_rocha_11@hotmail.com <galo0800>
2018-06-04 16:44:20 -0300

    Review 1.0 - Interface interligada

--- src/Controller/AppController.php
+++ src/Controller/AppController.php
@@ -53,4 +53,12 @@ class AppController extends Controller
         //$this->loadComponent('Security');
         //$this->loadComponent('Csrf');
     }
+    
+    public function beforeRender(Event $event)
+    {
+        $this->viewBuilder()->setTheme('AdminLTE');
+
+        // For CakePHP before 3.5
+        $this->viewBuilder()->theme('AdminLTE');
+    }
 }
